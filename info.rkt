60ad039e9f533fd22abc33ae4011f9f1aa891fd5
Pavel Panchekha <me@pavpanchekha.com>
2020-05-18 11:36:24 -0600

    Added package info

---
+++ info.rkt
@@ -0,0 +1,14 @@
+#lang info
+
+(define collection "rival")
+(define version "1.4")
+
+;; Packaging information
+
+(define pkg-desc "Interval arithmetic for real computation")
+(define pkg-authors
+  '("Pavel Panchekha" "Oliver Flatt"))
+
+;; Dependencies
+(define deps '(("base" #:version "7.0") "math-lib" "rackunit-lib"))
+(define build-deps '("rackunit-lib"))
