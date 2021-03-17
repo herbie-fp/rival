5e3b270196832fcf809b33b42d1d8d9bb9dedc5e
Pavel Panchekha <me@pavpanchekha.com>
2021-03-16 18:13:53 -0600

    New Mathematica runner

--- infra/run-mathematica.rkt
+++ infra/run-mathematica.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+; Issues at 32000 with (λ (B x) ...)
 
 (require math/bigfloat)
 (require math/bigfloat)
@@ -7,15 +8,16 @@
 (require "./interval-evaluate.rkt")
 (require "./run-mpfi.rkt")
 
-
-(define to-mathematica-function
+(define function->wolfram
   (make-hash
     `((pow . Power)
       (+ . Plus)
       (- . Subtract)
       (/ . Divide)
-      (sqrt . Surd)
+      (sqrt . Sqrt)
       (* . Times)
+      (fma . FMA)
+      (hypot . Hypot)
       (exp . Exp)
       (log . Log)
       (log10 . Log10)
@@ -61,116 +63,251 @@
 
 ;; exp2, expm1, 
 
-(define (transform-function func)
-  (cond
-    [(hash-has-key? to-mathematica-function func)
-     (string->symbol (string-append (symbol->string (hash-ref to-mathematica-function func)) "Wrapped"))]
-    [else 
-      #f]))
-
-
 (define (number->wolfram num)
-  (define split (string-split (number->string num) "e"))
-  (format "flconst[~a]"
-          (if
-           (equal? (length split) 1)
-           (first split)
-           (string-append (first split) "*^" (string-replace (second split) "+" "")))))
-  #;(format "Divide[~a, ~a]" (number->string (numerator exact-num)) (number->string (denominator exact-num)))
+  (define num2 (inexact->exact num))
+  (format "Divide[~a, ~a]" (numerator num2) (denominator num2)))
 
-(define (transform-wolfram expr hash)
-  (cond
-    [(list? expr)
-     (define transformed-name (transform-function (first expr)))
-     (define r (map (curryr transform-wolfram hash) (rest expr)))
-     (if (andmap identity (cons transformed-name r))
-         (format "~a[~a]" (transform-function (first expr))
-                         (string-join
-                           r
-                           ", "))
-        #f)]
-    [(hash-has-key? hash expr)
-     (hash-ref hash expr)]
-    [(number? expr)
-     (number->wolfram expr)]
-    [(equal? expr 'PI)
+(define (expr->wolfram expr)
+  (match expr
+    [(list op (app expr->wolfram args) ...)
+     (define fn (hash-ref function->wolfram op))
+     (format "checkReal[~a[~a]]" fn (string-join args ", "))]
+    ['PI
      "Pi"]
-    [else
-     (error (string-append "found constant or something " (~a expr)))]))
+    ['E
+     "E"]
+    [(? symbol?)
+     (regexp-replace #rx"[*._-]" (symbol->string expr) "AWeirdSymbol")]
+    [(? number?)
+     (number->wolfram expr)]))
 
-(define (prog->wolfram prog point)
-  (define hash (make-hash (map cons (program-variables prog) (map number->wolfram point))))
-  (transform-wolfram (program-body prog) hash))
+(define (program->wolfram prog)
+  (format "f[~a] := ~a\n"
+          (string-join (map (compose (curry format "~a_") expr->wolfram) (program-variables prog)) ", ")
+          (expr->wolfram (program-body prog))))
 
-(define todo empty)
-
-(define maxtime 0)
-
-(define (run-on-points port output rival-port point-count)  
-  (define all
-    (for/vector ([read-res (in-port read port)])
+(define (load-points port)
+  (define points
+    (for/list ([read-res (in-port read port)])
       read-res))
-  (define all-random
-    (for/vector ([i (in-range 1000)])
-      (vector-ref all (random (vector-length all)))))
-  (for ([read-res all] [i (in-range (vector-length all))])
-    (when (equal? (modulo i 1000) 0)
-      (display "Converted ")
-      (display point-count)
-      (displayln " points"))
-  
-      (match-define (list suite prog pt) read-res)
-      (define str (prog->wolfram prog pt))
-      (define before (current-inexact-milliseconds))
-      (define rival-res
-        (parameterize ([bf-precision 10000])
-          (interval-evaluate (program-body prog) (program-variables prog) pt #f)))
-      (define after (- (current-inexact-milliseconds) before))
-      (when (> after maxtime) (set! maxtime after))
-      
-      (when rival-res
-        (writeln (list suite prog pt rival-res) rival-port)
-        (write-todo str output))))
+  (for/hash ([group (group-by second points)])
+    (values (second (car group)) (map third group))))
 
-(define (write-todo item output)
-    (when item
-      (fprintf output "Print[\"\\\"\"],\n")
-      (fprintf output "V := Quiet[Catch[N[~a, 16], _SystemException]],\n" item)
-      (fprintf output "Print[ReturnIfReal[V]],\n")
-      (fprintf output "Print[\"\\\"\"],\n")
-      (fprintf output "ClearSystemCache[],\n")))
+(define math-path (find-executable-path "math"))
 
-(define (make-wrapped-functions port)
-  (for ([(key funcname) (in-hash to-mathematica-function)])
-       (if (equal? key 'sqrt)
-           (fprintf port "~aWrapped[xs___] := If[Greater[Length[Intersection[{xs}, PropagateSet]], 0], First[Intersection[{xs}, PropagateSet]], ReturnIfReal[~a[xs, 2]]],\n" funcname funcname)
-	   (fprintf port "~aWrapped[xs___] := If[Greater[Length[Intersection[{xs}, PropagateSet]], 0], First[Intersection[{xs}, PropagateSet]], ReturnIfReal[~a[xs]]],\n" funcname funcname))))
+(define (make-mathematica prog #:backup [backup #f])
+  (define-values (process m-out m-in m-err)
+    (subprocess #f #f #f math-path))
 
-(define (run-mathematica script-file output-port)
-  (define-values (process in out err) (subprocess output-port #f #f (find-executable-path "wolframscript") "-file" (build-path script-file)))
-  (subprocess-wait process))
+  (define buffer (make-bytes 65536 0))
+
+  (define (ffprintf fmt . vs)
+    (apply fprintf m-in fmt vs)
+    (when backup (apply fprintf backup fmt vs))
+    (flush-output m-in))
+
+  (ffprintf "~a\n" (call-with-input-file "headers.wls" port->string))
+  (ffprintf "~a\n" (program->wolfram prog))
+  (ffprintf "Print[\"Rival\" <> \"Ready\"]\n")
+  (let loop ([i 0])
+    (define step (read-bytes-avail! buffer m-out i))
+    (define s (bytes->string/latin-1 buffer #f 0 (+ i step)))
+    (if (regexp-match #rx".*RivalReady.*In\\[[0-9]+\\].*" s)
+        (eprintf "Mathematica started for ~a\n" prog)
+        (loop (+ i step))))
+
+  (values process m-out m-in m-err))
+
+(define (run-mathematica prog pts #:backup [backup #f])
+  (define-values (process m-out m-in m-err)
+    (make-mathematica prog #:backup backup))
+
+  (define buffer (make-bytes 65536 0))
+  (define (ffprintf fmt . vs)
+    (apply fprintf m-in fmt vs)
+    (when backup (apply fprintf backup fmt vs))
+    (flush-output m-in))
+
+  (define out
+    (for/list ([pt (in-list pts)])
+      (define start (current-inexact-milliseconds))
+      (ffprintf "TimeConstrained[FullForm[N[f[~a], 20]], 1]\n"
+                (string-join (map number->wolfram pt) ", "))
+      (let loop ([i 0])
+        (define step (read-bytes-avail!* buffer m-out i))
+        (define s (bytes->string/latin-1 buffer #f 0 (+ i step)))
+        (cond
+         [(> (- (current-inexact-milliseconds) start) 2000.0)
+          (eprintf "Killing and restarting Mathematica\n")
+          (eprintf "~s\n" s)
+          (define-values (process2 m-out2 m-in2 m-err2)
+            (make-mathematica prog #:backup backup))
+          (subprocess-kill process true)
+          (set! process process2)
+          (set! m-out m-out2)
+          (set! m-in m-in2)
+          (set! m-err m-err2)
+          (cons 10000.0 'timeout)]
+         [(string-contains? s "\nIn")
+          (let ([dt (- (current-inexact-milliseconds) start)])
+            (printf ".")
+            (flush-output)
+            (cons dt (parse-output s)))]
+         [else
+          (loop (+ i step))]))))
+  (ffprintf "Exit[]\n")
+  (subprocess-wait process)
+  (cons (subprocess-status process) out))
+
+(define (parse-output s)
+  (define lines (string-split s "\n" #:repeat? #t))
+  (with-handlers ([exn:misc:match? (λ (e)
+    (newline)
+    (printf "Could not parse results:\n")
+    (pretty-print lines)
+    (exit))])
+
+    (match-lines lines)))
+
+(define (match-lines lines)
+  (match lines
+    [(list
+      (regexp #rx"In\\[[0-9]+\\]:= ")
+      rest ...)
+     (match-lines rest)]
+    [(list
+      rest ...
+      (regexp #rx"In\\[[0-9]+\\]:= "))
+     (match-lines rest)]
+    [(list (regexp #rx"Out\\[[0-9]+\\]= \\$Aborted"))
+     'timeout]
+    [(list (regexp #rx"Out\\[[0-9]+\\]//FullForm= (.+)" (list _ x)))
+     x]
+    [(list
+      (regexp #rx"Out\\[[0-9]+\\]//FullForm= ")
+      " "
+      (regexp #rx"> +[0-9-](.+)" (list _ x)))
+     x]
+    [(list
+      "Throw::nocatch: Uncaught Throw[domain-error, BadValue] returned to top level."
+      _ ...)
+     'invalid]
+    [(list
+      "General::ovfl: Overflow occurred in computation."
+      _ ...)
+     'unsamplable]
+    [(list
+      "General::unfl: Underflow occurred in computation."
+      _ ...)
+     'unsamplable]
+    [(list
+      "General::nomem: "
+      "   The current computation was aborted because there was insufficient memory"
+      "    available to complete the computation."
+      _ ...)
+     (eprintf "Mathematica ran out of memory!\n")
+     'memory]
+    [(list
+      _ ...
+      (regexp #rx"Divide::indet: Indeterminate expression .*")
+      _ ...)
+     'unsamplable]
+    [(list
+      _ ...
+      (regexp #rx"Divide::infy: Infinite expression .*")
+      _ ...)
+     'unsamplable]
+    [(list
+      "Divide::infy: Infinite expression "
+      _ ...)
+     'unsamplable]
+    [(list
+      _ ...
+      (regexp #rx"Power::infy: Infinite expression .*")
+      _ ...)
+     'unsamplable]
+    [(list
+      _ ...
+      (regexp #rx"Infinity::indet: Indeterminate expression .*")
+      _ ...)
+     'unsamplable]
+    [(list
+      _ ...
+      "Infinity::indet: "
+      _ ...
+      (regexp #rx"Indeterminate expression .*")
+      _ ...)
+     'unsamplable]
+    [(list
+      _ ...
+      (regexp #rx"ArcTan::indet: Indeterminate expression .*")
+      _ ...)
+     'unsamplable]
+    [(list
+      (regexp #rx"General::stop: Further output of .*")
+      (regexp #rx" +will be suppressed during this calculation")
+      _ ...)
+     'unsamplable]
+    [(list
+      "N::meprec: Internal precision limit $MaxExtraPrecision = 3100."
+      _ ...)
+     'unknown]
+    ))
+
+(define (count-results out)
+  (define sampled 0)
+  (define invalid 0)
+  (define unsamplable 0)
+  (define unknown 0)
+  (define crash 0)
+  (define timeout 0)
+  (for ([val out])
+    (match (cdr val)
+      ['invalid
+       (set! invalid (add1 invalid))]
+      ['memory
+       (set! unknown (add1 unknown))
+       (set! crash (add1 crash))]
+      ['timeout
+       (set! unknown (add1 unknown))
+       (set! timeout (add1 timeout))]
+      ['unsamplable
+       (set! unsamplable (add1 unsamplable))]
+      ['unknown
+       (set! unknown (add1 unknown))]
+      [(regexp #rx"[0-9]+(\\.[0-9]+)?(`[0-9]*)?(\\*\\^-?[0-9]+)?")
+       (set! sampled (add1 sampled))]
+      ))
+  (list sampled invalid unsamplable unknown crash timeout))
+
+(define (add-results r1 r2)
+  (map + r1 r2))
 
 (module+ main
-  (command-line #:program "run-mpfi"
-    #:args (points-file script-file output-file rival-file)
-    (define rival-port (open-output-file rival-file #:exists 'replace))
-    (define script-port (open-output-file script-file #:exists 'replace))
-    (displayln "#!/usr/bin/env wolframscript" script-port)
-    (displayln "Block[{$MaxExtraPrecision = 500, $HistoryLength=0},{" script-port)
-    (displayln "flconst[x_] := SetPrecision[x, 3082]," script-port)
-    (displayln "PropagateSet := {True, False, Underflow[], Overflow[], \"domain-error\", \"warning\", Indeterminate, ComplexInfinity},\n" script-port)
-    (displayln "ReturnIfReal := Function[a, If[Greater[Length[Intersection[{a}, PropagateSet]], 0], First[Intersection[{a}, PropagateSet]], If[NumericQ[a], If[Internal`RealValuedNumericQ[a], a, \"domain-error\"], Block[{}, {Print[\"Bad value \"], Print[a], Exit[]}]]]],\n" script-port)
-    (make-wrapped-functions script-port)
-    (run-on-points (open-input-file points-file) script-port rival-port 0)
-    (displayln "Null\n" script-port)
-    (displayln "}]" script-port)
-    (flush-output script-port)
-    (close-output-port script-port)
-    (println "max time")
-    (println maxtime)
-    (println "Running mathematica on converted points")
-    (define output-port (open-output-file output-file #:exists 'replace))
-    (define before (current-inexact-milliseconds))
-    (run-mathematica script-file output-port)
-    (define after (current-inexact-milliseconds))
-    (displayln (format "~a milliseconds mathematica" (- after before)))))
+  (command-line #:program "run-mathematica"
+    #:args (points-file . skip-cmd)
+    (define skip
+      (if (> (length skip-cmd) 0)
+          (string->number (first skip-cmd))
+          0))
+    (define points (call-with-input-file points-file load-points))
+    (define results (list 0 0 0 0 0 0))
+    (for ([(prog pts) (in-hash points)] [n (in-naturals)])
+      (cond
+       [(> skip (length pts))
+        (set! results (list-set results 3 (+ (list-ref results 3) (length pts))))
+        (set! skip (- skip (length pts)))]
+       [else
+        (match-define (cons status out)
+                      (run-mathematica prog pts #:backup (open-output-file "mathematica.log" #:exists 'replace)))
+        (match status
+          [0
+           (define r2 (count-results out))
+           (set! results (add-results results r2))
+           (match-define (list sampled invalid unsamplable unknown crash timeout) results)
+           (eprintf "\nResults: ~a ok, ~a bad, ~a unsamplable, ~a unknown (~a crash, ~a timeout)\n"
+                    sampled invalid unsamplable unknown crash timeout)]
+          [_
+           (printf "Status: ~a\n" status)
+           (pretty-print out)
+           (exit)])]))))
