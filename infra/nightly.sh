a724caa663d61412029b290c3474d310466bdbb9
Pavel Panchekha <me@pavpanchekha.com>
2023-01-06 16:36:07 -0700

    Up the time limit and remove the old upload script

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -58,7 +58,7 @@ function all {
 function perf {
     clean
     echo "<!doctype html><pre>" > "$REPORTDIR"/index.html
-    racket time.rkt 100 >> "$REPORTDIR"/index.html
+    racket time.rkt 32000 | tee -a "$REPORTDIR"/index.html
     echo "</pre>" >> "$REPORTDIR"/index.html
 }
 
