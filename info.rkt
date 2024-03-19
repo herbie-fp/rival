68e37f5f3d85bcd652470f8a23120ef73ab171f6
Pavel Panchekha <me@pavpanchekha.com>
2024-03-18 21:45:58 -0600

    Clean up info.rkt

--- info.rkt
+++ info.rkt
@@ -2,13 +2,15 @@
 
 (define collection "rival")
 (define version "1.7")
+(define license "MIT License")
 
 ;; Packaging information
 
 (define pkg-desc "Interval arithmetic for real computation")
-(define pkg-authors '("Pavel Panchekha" "Oliver Flatt"))
+(define pkg-authors '("Pavel Panchekha" "Oliver Flatt" "Artem Yadrov"))
 
 (define compile-omit-paths '("infra"))
+(define test-omit-paths '("infra"))
 
 ;; Dependencies
 (define deps '(("base" #:version "8.0") "math-lib" "rackunit-lib"))
