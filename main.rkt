f4eaa8d98e08c649bed5ca2b10d258a84138539c
AYadrov <45910827+AYadrov@users.noreply.github.com>
2024-05-05 14:32:13 -0600

    fix for negative values

--- main.rkt
+++ main.rkt
@@ -515,10 +515,12 @@
               (ival-neg (ival-pow-pos (ival-exact-fabs x) y)) ; Use fabs in case of [x, 0]
               (ival-pow-pos (ival-exact-fabs x) y))
 
-          (let ([frac (rationalize (bigfloat->rational (ival-lo-val y)) (expt 2 (- (bf-precision))))])
-            (if (and (odd? (numerator frac)) (odd? (denominator frac)))
-                (ival-neg (ival-pow-pos (ival-exact-fabs x) y)) ; y is an odd fraction
-                ival-illegal)))
+          (if (bfnegative? (ival-lo-val y))
+            ival-illegal
+            (let ([frac (rationalize (bigfloat->rational (ival-lo-val y)) (expt 2 (- (bf-precision))))])
+                 (if (and (odd? (numerator frac)) (odd? (denominator frac)))
+                     (ival-neg (ival-pow-pos (ival-exact-fabs x) y)) ; y is an odd fraction
+                     ival-illegal))))
             
       ; Moreover, if we have (-x)^y, that's basically x^y U -(x^y).
       (let ([pospow (ival-pow-pos (ival-exact-fabs x) y)])
