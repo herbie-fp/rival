eeadfd56a5db66bf2a6d2100779adba52c182502
AYadrov <kowalski228@mail.ru>
2024-11-18 15:34:34 -0700

    a possible CI fix

--- eval/compile.rkt
+++ eval/compile.rkt
@@ -273,7 +273,7 @@
   ;; vector for adjusting precisions
   (define incremental-precisions (setup-vstart-precs instructions (length vars) roots discs))
   (define initial-precision
-    (+ (apply max (map discretization-target (rest discs))) (*base-tuning-precision*)))
+    (+ (argmax identity (map discretization-target (rest discs))) (*base-tuning-precision*)))
 
   (rival-machine (list->vector vars)
                  instructions
