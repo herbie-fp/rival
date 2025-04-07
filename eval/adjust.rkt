0cef0dc2b519db7bc6418b370e49103a03087a5e
AYadrov <kowalski228@mail.ru>
2025-04-06 21:43:30 -0600

    it gets more and more complicated.. and maybe faster

--- eval/adjust.rkt
+++ eval/adjust.rkt
@@ -168,11 +168,19 @@
     (vector-set! vprecs-new (- root-reg varc) (get-slack)))
 
   ; Step 1b. Checking if a operation should be computed again at all
+  ;   This step traverses instructions top-down and check whether a reevaluation is needed
+  ;   Reevaluation can be skipped if:
+  ;   1) the result is already exact
+  ;   2) the operation is a part of the path that has been reduced
+  ;   Once the path is fully reduced - vrepeats will store this path - no more reduction is needed
+  (define reduction-needed (not (*path-fully-reduced*)))
+  (define converged? #t)
   (when first-tuning-pass?
     (vector-fill! vrepeats #t)) ; vrepeats will be reused for 2nd+ tuning pass
+  
   (for ([root (in-vector rootvec)]
         #:when (>= root varc))
-    (vector-set! vrepeats (- root varc) #f)) ; #f - means it WILL be REEXECUTED
+    (vector-set! vrepeats (- root varc) #f))
   (for ([reg (in-vector vregs (- (vector-length vregs) 1) (- varc 1) -1)]
         [instr (in-vector ivec (- (vector-length ivec) 1) -1 -1)]
         [i (in-range (- (vector-length ivec) 1) -1 -1)]
@@ -180,7 +188,11 @@
         #:unless repeat?)
     (cond
       [(and (ival-lo-fixed? reg) (ival-hi-fixed? reg)) (vector-set! vrepeats i #t)]
-      [else (path-reduction vrepeats vregs varc instr i #:reexec-val #f)]))
+      [reduction-needed
+       (define-values (_ converged?*)
+         (path-reduction vrepeats vregs varc instr i #:reexec-val #f))
+       (set! converged? (and converged? converged?*))]))
+  (*path-fully-reduced* converged?)
 
   ; Step 2. Precision tuning
   (precision-tuning ivec vregs vprecs-new varc vstart-precs vrepeats vhint)
