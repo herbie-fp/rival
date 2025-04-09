75255e31f5e34424ee013eab7893dc6100e31068
Pavel Panchekha <me@pavpanchekha.com>
2025-04-09 12:33:26 -0600

    fmt

--- ops/pow.rkt
+++ ops/pow.rkt
@@ -105,9 +105,9 @@
     (ival (endpoint lo real-lo!)
           (endpoint hi real-hi!)
           (or xerr? yerr? (and (bfzero? (endpoint-val xlo)) (not (= y-class 1))))
-          (or xerr yerr (and (bfzero? (endpoint-val xhi))
-                             (= y-class -1)
-                             (not (bfzero? (endpoint-val yhi)))))))
+          (or xerr
+              yerr
+              (and (bfzero? (endpoint-val xhi)) (= y-class -1) (not (bfzero? (endpoint-val yhi)))))))
 
   (match* (x-class y-class)
     [(1 1) (mk-pow xlo ylo xhi yhi)]
